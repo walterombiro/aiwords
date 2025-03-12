5d22f7ffbc5003b9d2f0fcf95b05a4d36f9e3e75
Wombiro <wombiro@gmail.com>
2025-03-12 22:17:33 +0300

    Initial commit

---
+++ aiwords.py
@@ -0,0 +1,11 @@
+#an application to define any word.
+import time
+
+word=""
+print("WELCOME TO AN APP THAT CAN DEFINE ANY WORD")
+print("=========================================")
+print()
+print()
+word=input("Enter a word to define below")
+time.sleep(2)
+
